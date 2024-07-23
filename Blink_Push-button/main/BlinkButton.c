26e60c26392bdc31442b0f4c943b401d1e96f05a
MP-SoftwareDeveloper <morteza.pourmehdi.se@gmail.com>
2024-07-23 22:04:56 +0200

    VScode ESP32 S3 initial commit

---
+++ Blink_Push-button/main/BlinkButton.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//#include "esp_mac.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+ 
+void app_main(void)
+{
+  gpio_set_direction(GPIO_NUM_12,GPIO_MODE_DEF_INPUT);
+  gpio_set_pull_mode(GPIO_NUM_12,GPIO_PULLUP_ONLY);
+  gpio_set_direction(GPIO_NUM_13,GPIO_MODE_DEF_OUTPUT );
+
+  while (true)
+  {
+    //gpio_set_level(13,1);
+    if(gpio_get_level(GPIO_NUM_12))
+        gpio_set_level(GPIO_NUM_13,0);
+    else
+        gpio_set_level(GPIO_NUM_13,1);
+    /* code */
+
+    vTaskDelay(10);
+  }
+    
+}
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+A journey with ESP32 Picow S3 and VScode I'm using a tutorial on Youtube for configuration use this: 
+https://www.youtube.com/watch?v=XDDcS7HQNlI and for more examples use: https://www.youtube.com/watch?v=5IuZ-E8Tmhg 
+Follow the Video first you need to install esp-idf-tools-setup-espressif-ide-3.0.0-with-esp-idf-5.2.2.exe and follow 
+the instructions then install VScode
