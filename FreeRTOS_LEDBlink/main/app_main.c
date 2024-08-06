03169f0270ef886959be8f732276d03a9c369f65
MP-SoftwareDeveloper <morteza.pourmehdi.se@gmail.com>
2024-08-06 20:02:47 +0200

    FreeRTOS Blink LED GPIO13

---
+++ FreeRTOS_LEDBlink/main/app_main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h" 
+#include "driver/gpio.h"
+
+void blink(void)
+{
+    gpio_set_direction(GPIO_NUM_13,GPIO_MODE_OUTPUT);
+
+    while(1)
+    {
+        gpio_set_level(GPIO_NUM_13,0);
+        vTaskDelay(20);
+        gpio_set_level(GPIO_NUM_13,1 );
+        vTaskDelay(20);
+    }
+}
+void app_main(void)
+{
+    xTaskCreate(blink , "LED blink", 2048, NULL , 0, NULL);
+  
+}
